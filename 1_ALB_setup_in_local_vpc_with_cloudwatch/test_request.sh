810d5264d2dedeb168fdf46f24bc7c8ee9051c6e
Narayanan Krishnan <narayanan.k.1985@gmail.com>
2019-12-12 09:53:42 +0000

    Modified the test script to run very limited number of requests

--- 1_ALB_setup_in_local_vpc_with_cloudwatch/test_request.sh
+++ 1_ALB_setup_in_local_vpc_with_cloudwatch/test_request.sh
@@ -2,12 +2,13 @@
 
 LOGFILE="${0%%.sh}.log"
 RESULTFILE="${0%%.sh}.html"
+[ -f $RESULTFILE ] && rm $RESULTFILE
 exec >$LOGFILE
 exec 2>&1
 echo $LOGFILE
 echo "Bash version ${BASH_VERSION}..."
 URL="http://vaanu-alb-975587269.eu-west-2.elb.amazonaws.com:80"
-for i in {0..250}
+for i in {0..100}
    do
       echo "<h4> Request No: $i </h4>" >> $RESULTFILE
       curl -s $URL >> $RESULTFILE
--- 1_EC2_setup_in_local_vpc_with_cloudwatch/test_request.sh
+++ 1_EC2_setup_in_local_vpc_with_cloudwatch/test_request.sh
@@ -6,7 +6,8 @@ exec >$LOGFILE
 exec 2>&1
 echo $LOGFILE
 echo "Bash version ${BASH_VERSION}..."
-declare -a IP_ADDRESSES=("IP-1" "IP-2" "IP-3")
+declare -a IP_ADDRESSES=("18.130.239.221")
+#declare -a IP_ADDRESSES=("IP-1" "IP-2" "IP-3")
 for i in {0..250}
    do
       echo "<h4> Iteration No: $i </h4>" >> $RESULTFILE
